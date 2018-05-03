2d24b62c925a817aab1cf7de70e87c4a46ffe19e
Cesar Piangers <cesarpiangers@gmail.com>
2018-05-03 18:10:50 -0300

    plugin trim em desenvolvimento

--- trim.py
+++ trim.py
@@ -16,31 +16,19 @@ class Trim:
 
     def initGui(self):
         # 1 - CRIAR UM BOTÃO PARA ATIVAR A FERRAMENTA
+        settings = QSettings()
         self.trimAction = QAction(QIcon(":/plugins/Trim/tr.png"), u'Trim', self.iface.mainWindow())
         self.toolbar = self.iface.addToolBar(u'Trim tools')
         self.toolbar.addAction(self.trimAction)
-
         # 2 - CONECTAR O CLIQUE DO BOTÃO COM UM MÉTODO ("SLOT")
         self.trimAction.triggered.connect(self.trim)
-
-        # 3 - MÉTODO TRIM
-
-    def unload(self):
-        del self.toolbar
-        self.canvas.unsetMapTool(self.seletor)
-
-    '''
-        settings = QSettings()
-        # Create action
         self.freehand_edit = \
-            QAction(QIcon(":/plugins/freehandEditing/l.png"),
-                    "Freehand editing", self.iface.mainWindow())
         self.freehand_edit.setEnabled(False)
         self.freehand_edit.setCheckable(True)
-        # Add toolbar button and menu item
+        # Adiciona botão toolbar no menu 
         self.iface.digitizeToolBar().addAction(self.freehand_edit)
         self.iface.editMenu().addAction(self.freehand_edit)
-
+        # Padrões fixados
         self.spinBox = QDoubleSpinBox(self.iface.mainWindow())
         self.spinBox.setDecimals(3)
         self.spinBox.setMinimum(0.000)
@@ -55,15 +43,31 @@ class Trim:
             self.iface.digitizeToolBar().addWidget(self.spinBox)
         self.spinBox.setToolTip("Tolerance. Level of simplification.")
         self.spinBoxAction.setEnabled(False)
-
-        # Connect to signals for button behaviour
+        # Conecta o sinals do botão 
         self.freehand_edit.activated.connect(self.freehandediting)
         self.iface.currentLayerChanged['QgsMapLayer*'].connect(self.toggle)
         self.canvas.mapToolSet['QgsMapTool*'].connect(self.deactivate)
         self.spinBox.valueChanged[float].connect(self.tolerancesettings)
-
         # Get the tool
         self.tool = Trim(self.canvas)
+
+        
+
+    
+    def deactivate(self):
+        self.freehand_edit.setChecked(False)
+        if self.active:
+            self.tool.rbFinished['QgsGeometry*'].disconnect(self.createFeature)
+        self.active = False
+    
+    def unload(self):
+        self.iface.digitizeToolBar().removeAction(self.freehand_edit)
+        self.iface.digitizeToolBar().removeAction(self.spinBoxAction)
+        del self.toolbar
+        try:
+            self.canvas.unsetMapTool(self.seletor)
+        except:
+            pass
 
     def tolerancesettings(self):
         settings = QSettings()
@@ -74,7 +78,45 @@ class Trim:
         self.freehand_edit.setChecked(True)
         self.tool.rbFinished['QgsGeometry*'].connect(self.createFeature)
         self.active = True
-    '''    
+
+    def toggle(self):
+        mc = self.canvas
+        layer = mc.currentLayer()
+        if layer is None:
+            return
+
+        #Decide whether the plugin button/menu is enabled or disabled
+        if (layer.isEditable() and (layer.geometryType() == QGis.Line or
+                                    layer.geometryType() == QGis.Polygon)):
+            self.freehand_edit.setEnabled(True)
+            self.spinBoxAction.setEnabled(
+                layer.crs().projectionAcronym() != "longlat")
+            try:  # remove any existing connection first
+                layer.editingStopped.disconnect(self.toggle)
+            except TypeError:  # missing connection
+                pass
+            layer.editingStopped.connect(self.toggle)
+            try:
+                layer.editingStarted.disconnect(self.toggle)
+            except TypeError:  # missing connection
+                pass
+        else:
+            self.freehand_edit.setEnabled(False)
+            self.spinBoxAction.setEnabled(False)
+            if (layer.type() == QgsMapLayer.VectorLayer and
+                    (layer.geometryType() == QGis.Line or
+                     layer.geometryType() == QGis.Polygon)):
+                try:  # remove any existing connection first
+                    layer.editingStarted.disconnect(self.toggle)
+                except TypeError:  # missing connection
+                    pass
+                layer.editingStarted.connect(self.toggle)
+                try:
+                    layer.editingStopped.disconnect(self.toggle)
+                except TypeError:  # missing connection
+                    pass
+
+       
     
     def trim (self):
         # 4 - ATIVAR A FERRAMENTA DE SELEÇÃO "PERSONALIZADA"
@@ -86,25 +128,23 @@ class Trim:
 
     def doWork(self):
         print u'Deu certo!'
+        # 5 - PEGAR AS DUAS GEOMETRIAS SELECIONADAS E VERIFICAR SE EXISTE INTERSEÇÃO
+        for s in selecionadas.getFeatures():
+            if s[0].geometry().intersection(s[1].geometry()):
+                selecionadas.select(s[0].id()) 
+        # 6 - PEGA O PONTO DE INTERSEÇÃO E VERIFICA QUAl É O ULTIMO VERTICE DA PRIMEIRA GEOMETRIA SELECIONADA
+            
+            feat = layer.getFeatures()
 
-'''
-        intersections = []
-        mc = self.canvas
-        layer1 = mc.currentLayer()
-        layer2 = 
-        if layer is None:
-            return
-        if layer.geometryType() == QGis.Line:
-        elif geometry.intersection(0)
+            for feature in feat:
+                vertices = feature.geometry().asPolyline()
+                points = []
+
+                for v in vertices:
+                    points.append(v)
+    
         
 
-        # exemplo de interseção
-        
-        for a in layer1.getFeatures():
-            for b in layer2.getFeatures():
-                if a.geometry.intersects(b.geometry()):
-                    intersection = a.geometry().intersection(b.geometry())
-                    intersections.append(intersection.geometry().area())
 
     def selecaoMudou(self, added, removed, cleared):
         if len(added) > 2:
@@ -113,5 +153,4 @@ class Trim:
                 selecionados.append(added[i])
             
             self.iface.activeLayer().setSelectedFeatures(selecionados)
-
- '''           
+          
